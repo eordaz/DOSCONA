8848875c738358af17e227b10fe74f0f78446bf4
vsanchez <castaveronica76@yahoo.com.mx>
2018-01-17 17:58:50 -0600

    Se agrega firma de método para el insert y/o update de catálogos.

--- conacyt/conacyt-ejb/src/java/conacyt/beans/CatalogosConacytBean.java
+++ conacyt/conacyt-ejb/src/java/conacyt/beans/CatalogosConacytBean.java
@@ -15,10 +15,11 @@ import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
 
 /**
- * Clase de tipo Session Bean que contiene la logica de los servicios que se requieren
- * para las operaciones de los catálogos.
- * 
- * @author Edith Corina Ordaz Garnica <edith.ordaz@patronato.unam.mx> <edith.ordaz@gmail.com> 
+ * Clase de tipo Session Bean que contiene la logica de los servicios que se
+ * requieren para las operaciones de los catálogos.
+ *
+ * @author Edith Corina Ordaz Garnica <edith.ordaz@patronato.unam.mx>
+ * <edith.ordaz@gmail.com>
  */
 @Stateless
 public class CatalogosConacytBean implements CatalogosConacytBeanLocal {
@@ -57,10 +58,10 @@ public class CatalogosConacytBean implements CatalogosConacytBeanLocal {
         try {
             if (method.equals("getCatalogo")) {
                 result = getCatalogo(params);
+            } else if (method.equals("getCatMonedas")) {
+                result = insertorUpdateCatalogo(params);
                 /**
-                 * } else if (method.equals("getCatMonedas")) { result =
-                 * getCatMonedas(params); } else if
-                 * (method.equals("getCatDependencias")) { result =
+                 * else if (method.equals("getCatDependencias")) { result =
                  * getCatDependencias(params); } else if
                  * (method.equals("getCatSubDependencias")) { result =
                  * getCatSubDependencias(params);
@@ -75,48 +76,52 @@ public class CatalogosConacytBean implements CatalogosConacytBeanLocal {
     }
 
     /**
-     * Servicio genérico para obtener los registros de los catálogos, el modelo  
-     * de la base de datos nos permite generar un servicio para la obtención de  
+     * Servicio genérico para obtener los registros de los catálogos, el modelo
+     * de la base de datos nos permite generar un servicio para la obtención de
      * cualquier tipo de catálogo.
-     * 
+     *
      * @param params JSONObject p.ej. {"tabla":"nombre_del_catalogo"}
-     * @return JSONArray [ {"id_cat_cat_catalogos":2,"id_cat_tabla":"Administrador","valor":"Rol asignado para Administrador"},
-                           {"id_cat_cat_catalogos":3,"id_cat_tabla":"Dependencia","valor":"Rol asignada para Dependencias"}]
+     * @return JSONArray [
+     * {"id_cat_cat_catalogos":2,"id_cat_tabla":"Administrador","valor":"Rol
+     * asignado para Administrador"},
+     * {"id_cat_cat_catalogos":3,"id_cat_tabla":"Dependencia","valor":"Rol
+     * asignada para Dependencias"}]
      */
     private JSONArray getCatalogo(JSONObject params) {
         String methodStr = className + "::getCatalogo";
-        JSONArray result_query=null;
+        JSONArray result_query = null;
         JSONObject result = new JSONObject();
         String query_cat = null, query_cat_cat = null;
         Integer id_cat_tabla = null;
         try {
-            if (params != null && !params.isEmpty() && !params.isNullObject() && !params.getString("tabla").isEmpty()) {
-                query_cat = "SELECT " + conacyt_cfg.getString("column_id_tabla") +
-                            " FROM " + conacyt_cfg.getString("cat_tablas") + 
-                            " WHERE valor = \'" + params.getString("source") + "\'";
+            if (params != null && !params.isEmpty() && !params.isNullObject() && !params.getString("source").isEmpty()) {
+                query_cat = "SELECT " + conacyt_cfg.getString("column_id_tabla")
+                        + " FROM " + conacyt_cfg.getString("cat_tablas")
+                        + " WHERE valor = \'" + params.getString("source") + "\'";
                 id_cat_tabla = recordManager.executeQueryToID(query_cat, conacyt_cfg.getString("column_id_tabla"));
-                if(id_cat_tabla!=null && id_cat_tabla > 0){
+                if (id_cat_tabla != null && id_cat_tabla > 0) {
                     //LOGGER.log(Level.INFO, methodStr + ">Que hay en el id_cat_tabla. " + id_cat_tabla);
                     //Teniendo el identificador del catálogo a explotar se realiza la consulta al catálogo de catálogos dónde 
                     //solicitaremos todos los registros que esten asociados con el ID que enviamos como parámetro.
                     query_cat_cat = "SELECT * FROM " + conacyt_cfg.getString("cat_catalogos") + " WHERE id_cat_tabla = \'" + id_cat_tabla + "\'";
                     result_query = recordManager.executeQueryToArray(query_cat_cat);
                     for (int i = 0; i < result_query.size(); i++) {
-                        Object object = result_query.get(i);                       
-                        result.put(i,object);
+                        Object object = result_query.get(i);
+                        result.put(i, object);
                     }
                 } else {
-                    result_query = JSONArray.fromObject(new String("[{\"getCatalogo\":\"-1\",\"mensaje\":\">El catálogo que esta solicitando no se encuentra registrado en la base de datos.\"}]"));
                     LOGGER.log(Level.WARNING, methodStr + ">Error: > El catálogo que esta solicitando no se encuentra registrado en la base de datos." + params.getString("source"));
                 }
             } else {
-                result_query = JSONArray.fromObject(new String("[{\"getCatalogo\":\"-1\",\"mensaje\":\">Los parámetros que envía son nulos o vacíos.\"}]"));
                 LOGGER.log(Level.WARNING, methodStr + ">Error: > Los parámetros que envía son nulos o vacíos.");
             }
         } catch (Exception ex) {
-            result_query = JSONArray.fromObject(new String("[{\"getCatalogo\":\"-1\",\"mensaje\":\">Excepción al ejecutar el método.\"}]"));
             LOGGER.log(Level.SEVERE, methodStr + ">Excepción al ejecutar el método. ", ex);
         }
         return result_query;
+    }
+
+    private JSONArray insertorUpdateCatalogo(JSONObject params) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
